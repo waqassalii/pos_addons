63df2c2ab9e51dd671887d7e53d0ec788d72f5e4
WaqasAlii <waqasalirandhawa@gmail.com>
2024-02-08 02:18:04 +0500

    [ADD] click event on that button

--- pos_light/static/src/js/light_sample_button.js
+++ pos_light/static/src/js/light_sample_button.js
@@ -6,10 +6,19 @@ odoo.define("pos_light.OLSampleButton", function(require){
     const ProductScreen = require("point_of_sale.ProductScreen");
     // to register the template
     const Registries = require("point_of_sale.Registries");
+    // import the library
+    const { useListener } = require("@web/core/utils/hooks"); //note.2
 
     //create the custom class
     class OLSampleButton extends PosComponent{
+            setup(){
+            super.setup();
+            useListener('click', this.ol_sample_button_click);
+            } // note.1
 
+            ol_sample_button_click(){
+                console.log("Hello i am the click event ==============>>>>>>");
+            }
     }
     //    create a template for button
         //associate template named "OLSampleButton" with a JavaScript class named OLSampleButton
